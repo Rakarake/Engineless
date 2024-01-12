e9ff81262921e7574aef7f0cda674607fce72bc2
Rakarake <rak@rakarake.xyz>
2024-01-12 21:33:09 +0100

    Big boilerplate in obj

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "No game engines???";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nuget-packageslock2nix = {
+      url = "github:mdarocha/nuget-packageslock2nix/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, nuget-packageslock2nix, ...}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        #defaultPackage = pkgs.buildDotnetModule rec {
+        #  pname = "engineless";
+        #  version = "0.0.1";
+        #  src = ./.;
+        #  nugetDeps = nuget-packageslock2nix.lib {
+        #    system = system;
+        #    name = pname;
+        #    lockfiles = [
+        #      ./packages.lock.json
+        #    ];
+        #  };
+        #};
+        devShell = pkgs.mkShell { buildInputs = [ pkgs.dotnet-sdk_8 ]; };
+      }
+    );
+}
